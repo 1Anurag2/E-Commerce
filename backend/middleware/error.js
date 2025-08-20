0a403eecba03014989cc438b9b8a033f2608d962
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-08-20 12:33:33 +0530

    Error Handle Middleware

---
+++ backend/middleware/error.js
@@ -0,0 +1,18 @@
+import handleError from "../utils/handleError.js";
+const errorHandler = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+
+    // CastError: Handling invalid ObjectId
+    if (err.name === "CastError") {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new handleError(message, 400);
+    }
+
+    res.status(err.statusCode).json({
+        success: false,
+        message: err.message
+    })
+}
+
+export default errorHandler;
