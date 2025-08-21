68c85870006d75f403e60b228c733458252ce433
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-08-21 22:28:34 +0530

    productRoutes

--- backend/routers/productRoutes.js
+++ backend/routers/productRoutes.js
@@ -1,9 +1,11 @@
 import productController from "../controllers/productController.js";
 import express from "express";
+import {verifyUserAuth} from "../middleware/userAuth.js"
+
 const router = express.Router();
 
-router.route("/products").get(productController.getAllProducts).post(productController.createSingleProduct);
-router.route("/product/:id").get(productController.getSingleProduct).put(productController.updateProduct).delete(productController.deleteProduct);
+router.route("/products").get(verifyUserAuth,productController.getAllProducts).post(productController.createSingleProduct);
+router.route("/product/:id").get(verifyUserAuth,productController.getSingleProduct).put(productController.updateProduct).delete(productController.deleteProduct);
 
 
 
