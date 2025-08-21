7ca18a7b292847b9e736b5cdbdf64e7611331c29
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-08-21 22:26:41 +0530

    Error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,18 +1,26 @@
 import handleError from "../utils/handleError.js";
 const errorHandler = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
 
-    // CastError: Handling invalid ObjectId
-    if (err.name === "CastError") {
-        const message = `Resource not found. Invalid: ${err.path}`;
-        err = new handleError(message, 400);
-    }
+  // CastError: Handling invalid ObjectId
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new handleError(message, 400);
+  }
 
-    res.status(err.statusCode).json({
-        success: false,
-        message: err.message
-    })
-}
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `This ${Object.keys(
+      err.keyValue
+    )} already registered. Please Login to continue`;
+    err = new handleError(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
 
 export default errorHandler;
