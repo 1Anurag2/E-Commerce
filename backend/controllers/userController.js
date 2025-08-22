fc1f1820dba724b5704b60eaf4ea4c6290bcebb9
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-08-22 15:27:57 +0530

    User Controller implemented with user registration, login, logout, password reset, and user details retrieval functionalities.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,8 +2,11 @@ import handleAsyncError from "../middleware/handleAsyncError.js";
 import handleError from "../utils/handleError.js";
 import User from "../models/userModel.js";
 import { sendToken } from "../utils/jwtToken.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import crypto from "crypto";
 
-export const registerUser = handleAsyncError(async (req, res, next) => {
+//Register
+const registerUser = handleAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
 
   const user = await User.create({
@@ -47,8 +50,125 @@ const logout = handleAsyncError(async (req, res, next) => {
   });
 });
 
+//Forgot Password
+const resetPassword = handleAsyncError(async (req, res, next) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(new handleError("User not found", 404));
+  }
+let resetToken;
+try {
+    resetToken = user.getResetPasswordToken();
+    await user.save({ validateBeforeSave: false });
+  } catch (error) {
+    return next(new handleError("Error generating reset token", 500));
+  }
+  const resetUrl = `${req.protocol}://${req.get("host")}/api/v1/reset/${resetToken}`;
+  console.log(resetUrl);
+  const message = `Use the following link to reset your password : ${resetUrl} \n\n This link will expire in 30 minutes.\n\n
+  If you didn't request a password reset , please ignore this email.`;
+  try {
+    //send email
+    await sendEmail({
+      email: user.email,
+      subject: "Password Reset Request",
+      message
+    })
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new handleError("Email could not be sent", 500));
+  }
+});
+
+//Reset Password
+const resetPasswordHandler = handleAsyncError(async (req, res, next) => {
+  const { token } = req.params;
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+  if (!user) {
+    return next(new handleError("Invalid or expired token", 400));
+  }
+
+  const {password , confirmPassword} = req.body;
+  if(password !== confirmPassword){
+    return next(new handleError("Password and confirm password do not match", 400));
+  }
+  user.password = password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+  sendToken(user, 200, res);
+}); 
+
+//get user Details
+const getUserDetails = handleAsyncError(async (req,res,next)=>{
+  const user = await User.findById(req.user.id);
+  // console.log(req.user.id) 
+  res.status(200).json({
+    success:true,
+    user
+  })
+})
+
+//update password
+const updatePassword = handleAsyncError(async (req,res,next)=>{
+  const user = await User.findById(req.user.id).select("+password");
+  const {oldPassword , newPassword , confirmPassword} = req.body;
+  if(!oldPassword || !newPassword || !confirmPassword){
+    return next(new handleError("Please enter all fields",400));
+  }
+  const isPasswordValid = await user.verifyPassword(oldPassword);
+  if(!isPasswordValid){
+    return next(new handleError("Old password is incorrect",400));
+  }
+  if(newPassword !== confirmPassword){
+    return next(new handleError("New password and confirm password do not match",400));
+  }
+  user.password = newPassword;
+  await user.save();
+  sendToken(user,200,res);
+})
+
+//updating user profile
+const updateProfile = handleAsyncError(async (req,res,next)=>{
+  const {name , email} = req.body
+  const updateUserDetail ={
+    name,
+    email
+  }
+  const user = await User.findByIdAndUpdate(req.user.id , updateUserDetail , {
+    new:true,
+    runValidators:true,
+    useFindAndModify:false
+  })
+  res.status(200).json({
+    success:true,
+    message : "Profile Updated Successfully",
+    user
+  })
+})
+
 export default {
   registerUser,
   loginUser,
   logout,
+  resetPassword,
+  resetPasswordHandler,
+  getUserDetails,
+  updatePassword,
+  updateProfile
 };
