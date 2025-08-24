e369c15a31240a215faec00ea0aec7ba74ba334c
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-08-24 19:42:05 +0530

    Routers

--- backend/routers/productRoutes.js
+++ backend/routers/productRoutes.js
@@ -1,12 +1,46 @@
 import productController from "../controllers/productController.js";
 import express from "express";
-import {verifyUserAuth} from "../middleware/userAuth.js"
+import { roleBasedAccess, verifyUserAuth } from "../middleware/userAuth.js";
 
 const router = express.Router();
 
-router.route("/products").get(verifyUserAuth,productController.getAllProducts).post(productController.createSingleProduct);
-router.route("/product/:id").get(verifyUserAuth,productController.getSingleProduct).put(productController.updateProduct).delete(productController.deleteProduct);
+router.route("/products").get(productController.getAllProducts);
 
+router
+  .route("/admin/products")
+  .get(
+    verifyUserAuth,
+    roleBasedAccess("admin"),
+    productController.getAdminProducts
+  );
 
+router
+  .route("/admin/product/create")
+  .post(
+    verifyUserAuth,
+    roleBasedAccess("admin"),
+    productController.createSingleProduct
+  );
+
+router
+  .route("/admin/product/:id")
+  .put(
+    verifyUserAuth,
+    roleBasedAccess("admin"),
+    productController.updateProduct
+  )
+  .delete(
+    verifyUserAuth,
+    roleBasedAccess("admin"),
+    productController.deleteProduct
+  );
+
+router
+  .route("/product/:id")
+  .get(verifyUserAuth, productController.getSingleProduct);
+
+router
+  .route("/review")
+  .put(verifyUserAuth, productController.createReviewForProduct);
 
 export default router;
--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import userContoller from '../controllers/userController.js';
+import {roleBasedAccess, verifyUserAuth } from "../middleware/userAuth.js";
 const router = express.Router();
 
 router.route('/register').post(userContoller.registerUser);
 router.route('/login').post(userContoller.loginUser);
 router.route('/logout').post(userContoller.logout);
+router.route('/password/forgot').post(userContoller.resetPassword);
+router.route('/reset/:token').post(userContoller.resetPasswordHandler);
+router.route('/profile').post(verifyUserAuth,userContoller.getUserDetails)
+router.route('/password/update').post(verifyUserAuth,userContoller.updatePassword);
+router.route('/profile/update').post(verifyUserAuth,userContoller.updateProfile);
+router.route('/admin/users').get(verifyUserAuth,roleBasedAccess('admin'),userContoller.getUserList);
+router.route('/admin/users/:id').get(verifyUserAuth,roleBasedAccess('admin'),userContoller.getSingleUser)
+    .put(verifyUserAuth,roleBasedAccess('admin'),userContoller.updateUserRole)
+    .delete(verifyUserAuth,roleBasedAccess('admin'),userContoller.deleteUser);
 
-export default router;
+export default router; 
