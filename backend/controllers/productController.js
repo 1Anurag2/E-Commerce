b188af42d027eab4d26ae34407011a04619e5bc4
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-08-27 22:58:53 +0530

    Product controller Completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,6 +3,7 @@ import handleError from "../utils/handleError.js";
 import handleAsyncError from "../middleware/handleAsyncError.js";
 import APIFunctionality from "../utils/apiFunctionality.js";
 
+// Get All Products
 const getAllProducts = handleAsyncError(async (req, res, next) => {
     // console.log(req.query);
     const resultPerPage = 10; 
@@ -32,18 +33,21 @@ const getAllProducts = handleAsyncError(async (req, res, next) => {
         totalPages,
         currentPage: page,
     });
-});
+}); 
 
+// Create Product 
 const createSingleProduct = handleAsyncError(async (req, res, next) => {
-  // const { name, image,category, description, price, stock , numOfReviews ,  } = req.body;
-  try {
+ req.body.user =  req.user.id;
+//  console.log(req.user);
+ 
     const product = await Product.create(req.body);
-    res.status(201).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+    res.status(201).json({
+        success: true,
+        product,
+    });
 });
 
+// Get Single Product Details
 const getSingleProduct = handleAsyncError(async (req, res, next) => {
   const { id } = req.params;
   try {
@@ -57,6 +61,7 @@ const getSingleProduct = handleAsyncError(async (req, res, next) => {
   }
 });
 
+// Update Product
 const updateProduct = handleAsyncError(async (req, res, next) => {
   const productData = await Product.findById(req.params.id);
   if (!productData) {
@@ -74,6 +79,7 @@ const updateProduct = handleAsyncError(async (req, res, next) => {
     .json({ success: true, message: "Product updated successfully", product });
 });
 
+//Delete Product
 const deleteProduct = handleAsyncError(async (req, res, next) => {
   const { id } = req.params;
   try {
@@ -87,10 +93,110 @@ const deleteProduct = handleAsyncError(async (req, res, next) => {
   }
 });
 
+//Creating and Updating Product Review
+const createReviewForProduct = handleAsyncError(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+     name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user.id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user.id.toString()) {
+        (rev.rating = rating), (rev.comment = comment);
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  product.ratings = product.reviews.reduce(
+    (acc, item) => item.rating + acc,
+    0
+  ) / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+//Getting reviews
+const getProductReviews = handleAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+  if (!product) { 
+    return next(new handleError("Product not found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+//Deleting Review
+const deleteReview = handleAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+  if (!product) {
+    return next(new handleError("Product not found", 404));
+  }
+  const reviews = product.reviews.filter(review=>review._id.toString() !== req.query.id.toString());  
+  console.log(reviews)
+  let sum = 0;
+  reviews.forEach((review) => {
+    sum += review.rating;
+  });
+  const ratings = reviews.length>0?sum / reviews.length:0;
+  const numOfReviews = reviews.length;
+  await Product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+  res.status(200).json({
+    success: true,
+    message: "Review deleted successfully", 
+  });
+})
+// Admin - Getting all products
+const getAdminProducts = handleAsyncError(async (req, res, next) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+
+ 
 export default {
   getAllProducts,
   getSingleProduct,
   createSingleProduct,
   updateProduct,
   deleteProduct,
+  getAdminProducts,
+  createReviewForProduct,
+  getProductReviews, 
+  deleteReview
+
 };
