7d8f229e83cd254959b79fdd65b7d2b3470fbbfe
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-09-08 15:16:21 +0530

    Product component updated

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,31 +1,44 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../componentStyles/Product.css";
 import Rating from "./Rating";
 function Product({ product }) {
-    const [rating, setRating] =useState(0);
-    const handleRatingChange = (newRating) => {
-      setRating(rating);
-      console.log(`Rating changed to: ${newRating}`);
-    }
+  const [rating, setRating] = useState(0);
+  const handleRatingChange = (newRating) => {
+    setRating(rating);
+    console.log(`Rating changed to: ${newRating}`);
+  };
 
   return (
-    <Link to={product.id} className="product_id">
+    <Link to={`/product/${product._id}`} className="product_id">
       <div className="product-card">
-        <img src={product.image[0]} alt="Product" />
+        <img
+          src={
+            product.images && product.images.length > 0
+              ? product.images[0].url
+              : "/placeholder.png"
+          }
+          alt={product.name}
+          className="product-image-card"
+        />{" "}
         <div className="product-details">
-          <h3 className="product-title">{product.title}</h3>
+          <h3 className="product-title">{product.name}</h3>
           <p className="product-price">
             <strong>Price </strong>
             {product.price}/-
           </p>
           <div className="rating_container">
-            <Rating value={product.rating} onRatingChange={handleRatingChange} disabled ={true}/>
+            <Rating
+              value={product.ratings}
+              onRatingChange={handleRatingChange}
+              disabled={true}
+            />
           </div>
           <div className="productCardSpan">
-            {product.numOfReviews} {product.numOfReviews > 1 ? "Reviews" : "Review"}
+            {product.numOfReviews}{" "}
+            {product.numOfReviews > 1 ? "Reviews" : "Review"}
           </div>
-          <button className="add-to-cart">Add to Cart</button>
+          <button className="add-to-cart">View Details</button>
         </div>
       </div>
     </Link>
