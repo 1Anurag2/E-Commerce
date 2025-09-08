edcaabf54cd406fedeff28e4840b8b0a299f5eef
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-09-08 15:17:22 +0530

    update pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,53 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../pageStyles/Home.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ImageSlider from "../components/ImageSlider";
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import PageTitle from "../components/PageTitle";
+import { useSelector, useDispatch } from "react-redux";
+import { getProduct , removeError} from "../features/products/productSlice"; 
+import { toast } from "react-toastify";
 
-const products = [
-  {
-    id: 1,
-    title: "Laptop",
-    price: 49560,
-    image: ["https://m.media-amazon.com/images/I/71YpUx0XFdL._AC_SL1500_.jpg"],
-    rating: 5,
-    numOfReviews: 25,
-  },
-
-  {
-    id: 2,
-    title: "Sony TV",
-    price: 29999,
-    image: ["https://m.media-amazon.com/images/I/71O52HAQvUL.jpg"],
-    rating: 3,
-    numOfReviews: 5,
-  },
-  {
-    id: 3,
-    title: "Camera",
-    price: 35000,
-    image: [
-      "https://tse3.mm.bing.net/th/id/OIP.woHIMcrWnNWpCwV4alxyngHaE8?pid=Api&P=0&h=180",
-    ],
-    rating: 2,
-    numOfReviews: 1,
-  },
-];
 function Home() {
+  const { loading, error, products, productCount } = useSelector(
+    (state) => state.product
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProduct());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if(error){
+      toast.error(error.message,{position:"top-center",autoClose:3000})
+      dispatch(removeError())
+    }
+  }, [dispatch,error]);
   return (
     <>
+    <Loader/>
+    {loading?(<Loader/>) : (<>
+      <PageTitle title="Home - E-Commerce" />
       <Navbar />
       <ImageSlider />
       <div className="home-container">
         <h2 className="home-heading">Trending Products</h2>
-        <div className="home-product-container">
-          {products.map((product, index) => (
-            <Product product={product} key={index} />
-          ))}
-        </div>
+
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          <div className="home-product-container">
+            {products &&
+              products.map((product, index) => (
+                <Product product={product} key={index} />
+              ))}
+          </div>
+        )}
       </div>
       <Footer />
+    </>)}
     </>
   );
 }
