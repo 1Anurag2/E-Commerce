2c729bd2afb10b97e7dd8bc2d269736a1540d28b
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-09-14 00:32:35 +0530

    main file for productSlice and userSlice

---
+++ frontend/src/app/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from '../features/products/productSlice'
+import userReducer from '../features/user/userSlice' 
+const store = configureStore({
+  reducer: {
+    product:productReducer,
+    user:userReducer
+  },
+});
+
+export default store;
