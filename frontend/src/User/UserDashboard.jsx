cb205257e77a1993c28a7756e94db2901e683d10
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-09-14 00:27:37 +0530

    User dashboard

---
+++ frontend/src/User/UserDashboard.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "../UserStyles/UserDashboard.css";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { logout, removeSuccess } from "../features/user/userSlice";
+
+function UserDashboard({ user }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [menuVisible, setMenuVisible] = useState(false);
+  function toggleMenu() {
+    setMenuVisible(!menuVisible);
+  }
+
+  function orders() {
+    navigate("/orders/user");
+  }
+
+  function profile() {
+    navigate("/profile");
+  }
+
+  function logoutUser() {
+    dispatch(logout())
+      .unwrap()
+      .then(() => {
+        toast.success("Logged out successfully", {
+          position: "top-center",
+          autoClose: 2000,
+        });
+        dispatch(removeSuccess());
+        navigate("/login");
+      })
+      .catch((error) => {
+        toast.error(error.message || "Logout Failed", {
+          position: "top-center",
+          autoClose: 2000,
+        });
+      });
+  }
+
+  function dashboard() {
+    navigate("/admin/dashboard");
+  }
+
+  const options = [
+    { name: "Orders", funcName: orders },
+    { name: "Account", funcName: profile },
+    { name: "Logout", funcName: logoutUser },
+  ];
+
+  if (user.role === "admin") {
+    options.unshift({ name: "Admin Dashboard", funcName: dashboard });
+  }
+
+  return (
+    <>
+      <div
+        className={`overlay ${menuVisible ? "show" : ""}`}
+        onClick={toggleMenu}
+      ></div>
+      <div className="dashboard-container">
+        <div className="profile-header" onClick={toggleMenu}>
+          <img
+            src={user?.avatar?.url ? user.avatar.url : "./images/profile.jpg"}
+            alt="profile"
+            className="profile-avatar"
+          />
+          <span className="profile-name">{user?.name || "User"}</span>
+        </div>
+        {menuVisible && (
+          <div className="menu-options">
+            {options.map((item) => (
+              <button
+                key={item.name}
+                className="menu-option-btn"
+                onClick={item.funcName}
+              >
+                {item.name}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default UserDashboard;
