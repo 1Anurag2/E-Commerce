7349181b2cef29f487efb0a1dcb3e37aa30e3b79
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-10-22 21:09:27 +0530

    paymentRoutes implemented

---
+++ backend/routers/paymentRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { verifyUserAuth } from '../middleware/userAuth.js';
+import { paymentVerification, processPayment, sendApiKey } from '../controllers/paymentController.js';
+const router = express.Router();
+
+router.route('/payment/process').post(verifyUserAuth,processPayment);
+router.route('/getKey').get(verifyUserAuth,sendApiKey);
+router.route('/paymentVerification').post(paymentVerification);
+
+export default router;
